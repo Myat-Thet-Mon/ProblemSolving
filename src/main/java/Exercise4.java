70c10bd7d4949168ddc2e100e8a288260bd41943
Myat-Thet-Mon <89791069+Myat-Thet-Mon@users.noreply.github.com>
2021-08-31 11:13:27 +0630

    Update Exercise4.java

--- src/main/java/Exercise4.java
+++ src/main/java/Exercise4.java
@@ -2,5 +2,20 @@
 /*
 
  */
+import java.util.Scanner;
 public class Exercise4 {
+  public static void main(String[]args){
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter orignal amount");
+    double orignalAmount=input.nextDouble();
+    System.out.println("Enter anual interest");
+    double anual=input.nextDouble();
+    double interest=anual/12;
+    System.out.println("Enter given amount");
+    double givenAmount=input.nextDouble();
+    double interestAmount=(originalAmount/100)*interest;
+    orignalAmount=orignalAmount-(givenAmount-intestAmount);
+    System.out.println(interestAmount);
+    System.out.println(orignalAmount);
+  }
 }
