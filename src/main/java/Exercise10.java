86970d89f99b7567f7474a32114004d1b727048a
Myat-Thet-Mon <89791069+Myat-Thet-Mon@users.noreply.github.com>
2021-08-31 11:28:21 +0630

    Update Exercise10.java

--- src/main/java/Exercise10.java
+++ src/main/java/Exercise10.java
@@ -1,6 +1,19 @@
 
 /*
-
+cent 
  */
+import java.util.Scanner;
 public class Exercise10 {
+ public static void main(String[]args){
+   Scanner input=new Scanner(System.in);
+   System.out.println("Enter cent");
+   int cent=input.nextInt();
+   int newOne=cent;
+   int quater=newOne/25;
+   newOne=newOne%25;
+   int dime=newOne/10;
+   newOne=newOne%10;
+   int nickle=newOne/5;
+   
+ }
 }
