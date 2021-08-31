2e4a15f762f65c91c47bbe3bb1542bdb54db6787
Myat-Thet-Mon <89791069+Myat-Thet-Mon@users.noreply.github.com>
2021-08-31 10:35:58 +0630

    Update Exercise3.java

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -11,6 +11,19 @@ displays it, one digit per line, like so
 public class Exercise3 {
     public static void main(String[] args) {
         int num=1998;
+        int newOne=num;
+        int a=newOne/1000;
+        System.outprintln(a);
+        newOne=newOne%1000;
+        int b=newOne/100;
+        System.outprintln(b);
+        newOne=newOne%100;
+        int c=newOne/10;
+        System.outprintln(c);
+        newOne=newOne%10;
+        int d=newOne/1;
+        System.out.println("Original number is "+num);
+        
     }
 }
 
